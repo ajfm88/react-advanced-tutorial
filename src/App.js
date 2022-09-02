807b87a186f57f56e130ba638b463433753fe135
unknown <ajfoucaultmo@mail.usf.edu>
2022-09-02 11:56:46 -0700

    06. UseState - Object Example

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Setup from './tutorial/01-useState/3-useState-array';
+import Setup from './tutorial/01-useState/4-useState-object';
 function App() {
   return (
     <div className='container'>
