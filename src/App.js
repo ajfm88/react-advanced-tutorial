4b537a981239b127389af31dd191e26563430681
unknown <ajfoucaultmo@mail.usf.edu>
2022-09-02 11:03:02 -0700

    05. UseState - Array Example

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Setup from './tutorial/01-useState/2-useState-basics';
+import Setup from './tutorial/01-useState/3-useState-array';
 function App() {
   return (
     <div className='container'>
