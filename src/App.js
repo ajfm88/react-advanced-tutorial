d2a06fb2c57aacede4d0b4a3fbee3878cda0da2f
unknown <ajfoucaultmo@mail.usf.edu>
2022-09-03 13:07:11 -0700

    08. Simple Counter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Setup from './tutorial/01-useState/4-useState-object';
+import Setup from './tutorial/01-useState/5-useState-counter';
 function App() {
   return (
     <div className='container'>
