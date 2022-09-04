585370410cf6ab2db9be2e43700d4247d1a7ae10
unknown <ajfoucaultmo@mail.usf.edu>
2022-09-04 11:01:26 -0700

    10. useEffect - Basics

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Setup from './tutorial/01-useState/5-useState-counter';
+import Setup from './tutorial/02-useEffect/1-useEffect-basics';
 function App() {
   return (
     <div className='container'>
