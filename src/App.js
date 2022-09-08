2a9443420b8eb2be3f9a9286be3d7c8f359f298a
unknown <ajfoucaultmo@mail.usf.edu>
2022-09-07 19:23:20 -0700

    15. Multiple Returns

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Setup from './tutorial/02-useEffect/3-useEffect-fetch-data';
+import Setup from './tutorial/03-conditional-rendering/1-multiple-returns';
 function App() {
   return (
     <div className='container'>
