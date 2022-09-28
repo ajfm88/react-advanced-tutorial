4ec69fe8cb99f9d1d46a1bc5834a799957926a2c
unknown <ajfoucaultmo@mail.usf.edu>
2022-09-27 18:55:42 -0700

    37. & 38. useMemo & useCallback - Fetch Example

--- src/tutorial/12-memo-useMemo-useCallback/index.js
+++ src/tutorial/12-memo-useMemo-useCallback/index.js
@@ -1,12 +1,22 @@
-import React, { useState, useEffect, useCallback, useMemo } from 'react';
-import { useFetch } from '../09-custom-hooks/2-useFetch';
+import React, { useState, useCallback, useMemo } from 'react';
+import { useFetch } from '../../9-custom-hooks/final/2-useFetch';
 
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/javascript-store-products';
 
 // every time props or state changes, component re-renders
-
+const calculateMostExpensive = (data) => {
+  return (
+    data.reduce((total, item) => {
+      const price = item.fields.price;
+      if (price >= total) {
+        total = price;
+      }
+      return total;
+    }, 0) / 100
+  );
+};
 const Index = () => {
   const { products } = useFetch(url);
   const [count, setCount] = useState(0);
@@ -16,22 +26,28 @@ const Index = () => {
     setCart(cart + 1);
   }, [cart]);
 
+  const mostExpensive = useMemo(
+    () => calculateMostExpensive(products),
+    [products]
+  );
   return (
     <>
       <h1>Count : {count}</h1>
       <button className='btn' onClick={() => setCount(count + 1)}>
         click me
       </button>
-      <h1 style={{ marginTop: '3rem' }}>cart: {cart}</h1>
+      <h1 style={{ marginTop: '3rem' }}>cart : {cart}</h1>
+      <h1>Most Expensive : ${mostExpensive}</h1>
       <BigList products={products} addToCart={addToCart} />
     </>
   );
 };
 
 const BigList = React.memo(({ products, addToCart }) => {
-  useEffect(() => {
-    console.log('big list called');
-  });
+  // useEffect(() => {
+  //   console.count('hello from big list');
+  // });
+
   return (
     <section className='products'>
       {products.map((product) => {
@@ -47,20 +63,20 @@ const BigList = React.memo(({ products, addToCart }) => {
   );
 });
 
-const SingleProduct = ({ fields }) => {
-  useEffect(() => {
-    console.log('big list called');
-  });
+const SingleProduct = ({ fields, addToCart }) => {
   let { name, price } = fields;
   price = price / 100;
   const image = fields.image[0].url;
 
+  // useEffect(() => {
+  //   console.count('hello from product');
+  // });
   return (
     <article className='product'>
       <img src={image} alt={name} />
       <h4>{name}</h4>
       <p>${price}</p>
-      <button onClick={addToCart}></button>
+      <button onClick={addToCart}>add to cart</button>
     </article>
   );
 };
